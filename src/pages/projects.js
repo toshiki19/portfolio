2d58b3675582700b3cb6598490008491f47fb672
toshiki <toshiki.u.19@gmail.com>
2023-05-11 23:25:49 +0900

    articlesぺーじの2記事作成

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,9 @@ import Link from 'next/link'
 import { GithubIcon } from '@/components/icons'
 import Image from 'next/image'
 import project_1 from '../../public/images/projects/crypto-screener-cover-image.jpg'
+import { motion } from 'framer-motion';
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
   return (
@@ -15,7 +18,10 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
     rounded-br-3xl'/>
     <Link href={link} target="_blank"
     className='w-1/2 cursor-printer overflow-hidden rounded-lg'>
-      <Image src={img} alt={title} className='w-full h-auto' />
+      <FramerImage src={img} alt={title} className='w-full h-auto' 
+      whileHover={{scale:1.05}}
+      transition={{duration:0.2}}
+      />
     </Link>
 
     <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
@@ -44,7 +50,10 @@ const Project = ({title, type, img, link, github}) => {
       rounded-br-3xl'/>
       <Link href={link} target="_blank"
       className='w-full cursor-printer overflow-hidden rounded-lg'>
-        <Image src={img} alt={title} className='w-full h-auto' />
+        <FramerImage src={img} alt={title} className='w-full h-auto' 
+        whileHover={{scale:1.05}}
+        transition={{duration:0.2}}
+        />
       </Link>
 
       <div className='w-full flex flex-col items-start justify-between mt-4'>
